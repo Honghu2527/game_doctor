e7de4f2f16422085cf84e532f07f0478eacc9c57
Honghu2527 <honghuzhang2527@gmail.com>
2026-09-23 02:02:26 +0200

    feat(minigame): add native WeChat game bootstrap

---
+++ minigame/game.js
@@ -0,0 +1,20 @@
+/* 微信小游戏原生入口 */
+GameGlobal.window = GameGlobal;
+GameGlobal.global = GameGlobal;
+
+require("./data/backgrounds.js");
+require("./data/schools.js");
+require("./data/events.js");
+require("./data/school_system.js");
+require("./data/schools_expansion.js");
+require("./data/career_tree.js");
+require("./data/college_route.js");
+require("./data/specialty_system.js");
+require("./data/education_system.js");
+require("./data/language_system.js");
+require("./data/job_system.js");
+require("./data/item_system.js");
+require("./data/opportunity_flows.js");
+require("./data/college_opportunities.js");
+
+require("./src/main.js");
