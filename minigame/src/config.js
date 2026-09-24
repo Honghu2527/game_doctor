0d2ce12cc5e93c3d975847d40866f18321548af4
Honghu2527 <honghuzhang2527@gmail.com>
2026-09-24 12:18:38 +0200

    config(minigame): pin production cloud environment

--- minigame/src/config.js
+++ minigame/src/config.js
@@ -4,6 +4,6 @@ module.exports = {
   rewardedAdUnitId: "",
   messageBoardEnabled: true,
   /* 留空时使用开发者工具当前选择的云开发环境；也可填写具体 envId。 */
-  cloudEnvId: "",
+  cloudEnvId: "cloud1-d3gkn6cbq0ed329b5",
   reviewSafe: true
 };
